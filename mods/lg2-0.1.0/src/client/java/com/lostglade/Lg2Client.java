9e2f65815f3e469a86cb9e79b5b245d3e82aa0d9
Kilka <alak6543@gmail.com>
2026-03-03 20:16:12 +0300

    Server

---
+++ mods/lg2-0.1.0/src/client/java/com/lostglade/Lg2Client.java
@@ -0,0 +1,10 @@
+package com.lostglade;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Lg2Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
