01cb6491da39de638b932e08f565c38d05240605
march-taylor <mart.core.v3@gmail.com>
2026-07-16 19:21:48 +0300

    исправил звуки шерсти

--- mods/lg2-0.1.0/src/main/java/com/lostglade/block/RainbowCarpetBlock.java
+++ mods/lg2-0.1.0/src/main/java/com/lostglade/block/RainbowCarpetBlock.java
@@ -6,11 +6,7 @@ import eu.pb4.polymer.blocks.api.PolymerBlockResourceUtils;
 import eu.pb4.polymer.blocks.api.PolymerTexturedBlock;
 import eu.pb4.polymer.core.api.block.PolymerBlock;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
-import net.minecraft.core.BlockPos;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CarpetBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -42,8 +38,4 @@ public final class RainbowCarpetBlock extends CarpetBlock implements PolymerBloc
 		return this.fallbackState;
 	}
 
-	@Override
-	protected void spawnDestroyParticles(Level level, Player player, BlockPos pos, BlockState state) {
-		level.levelEvent(player, 2001, pos, Block.getId(Blocks.WHITE_CARPET.defaultBlockState()));
-	}
 }
